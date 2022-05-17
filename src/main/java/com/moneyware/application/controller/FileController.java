9b23595e665d4bcd53ba03faf813cbfe10a892af
thongeanand <67955260+thongeanand@users.noreply.github.com>
2022-05-17 15:22:38 +0530

    Update FileController.java

--- src/main/java/com/moneyware/application/controller/FileController.java
+++ src/main/java/com/moneyware/application/controller/FileController.java
@@ -36,7 +36,6 @@ public class FileController {
 
             return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
         } catch (Exception e) {
-            e.printStackTrace();
             message = "Could not upload the file: " + file.getOriginalFilename() + "!";
             return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
         }
